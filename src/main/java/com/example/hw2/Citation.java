65607af3727091a5c034064632976a56c24acece
Nikolay Vergeles <Vergeles4Nikolay@gmail.com>
2023-04-14 20:53:17 +0300

    add Lombok @Data

--- src/main/java/com/example/hw2/Citation.java
+++ src/main/java/com/example/hw2/Citation.java
@@ -1,16 +1,10 @@
 package com.example.hw2;
 
 import jakarta.validation.constraints.Size;
-
+import lombok.Data;
+@Data
 public class Citation {
     @Size(max = 30)
+
     private String citation;
-
-    public String getCitation() {
-        return citation;
-    }
-
-    public void setCitation(String citation) {
-        this.citation = citation;
-    }
 }
